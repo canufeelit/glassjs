229bad51e525d40b7992f4a51b0eb69a0224f36a
DerekB <derek@motionstrand.com>
2016-07-27 15:03:54 -0700

    initial working version

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # glassjs
 jQuery library for window resize events.
+
+#### init
+```
+glass();
+```
+
+#### defaults
+```
+var DEFAULTS = {
+	'sm': [0, 768],
+	'med': [769, 1024],
+	'lrg': [1025] 
+};
+```
+
+#### init with custom breakpoints
+```
+glass({
+	'xsm': [0, 768],
+	'sm': [769, 992],
+	'med': [993, 1200],
+	'lrg': [1200]
+});
+```
+
+#### listen for events
+```
+$(window).on('glass.sm', function(){
+	// window width in range: [769, 992]
+	...
+});
+```
---
+++ src/glass.js
@@ -0,0 +1,69 @@
+/**
+ * glassjs - jQuery library for window resize events.
+ * @version v0.0.1
+ * @link https://github.com/derekborland/glassjs#readme
+ * @license MIT
+ */
+;(function($, window) {
+	
+	var $win;
+	var options;
+	var windowWidth;
+	var lastEvent;
+	var debounceTime = 400;
+	
+	var DEFAULTS = {
+		'sm': [0, 768],
+		'med': [769, 1024],
+		'lrg': [1025] 
+	};
+	
+	function Glass(options) {
+		$win = $(window);
+		
+		if(!options) options = DEFAULTS;
+		if(options) options = $.extend({}, DEFAULTS, options);
+		
+		// debounce
+		function debounce(func, wait, immediate) {
+			var timeout;
+			return function() {
+				var context = this, args = arguments;
+				var later = function() {
+					timeout = null;
+					if(!immediate) func.apply(context, args);
+				}
+				var callNow = immediate && !timeout;
+				clearTimeout(timeout);
+				timeout = setTimeout(later, wait);
+				if(callNow) func.apply(context, args);
+			};
+		}
+		
+		// window resize event
+		$win.resize(debounce(function() {
+			windowWidth = $win.width();
+			for(var key in options) { 
+				
+				if(options[key].length === 2 && windowWidth >= options[key][0] && windowWidth <= options[key][1]) {
+					// console.log(key);
+					if(lastEvent === key) return;
+					lastEvent = key;
+					$win.trigger('glass.' + key, [windowWidth]);
+				}
+				
+				if(options[key].length === 1 && windowWidth >= options[key][0]) {
+					// console.log(key);
+					if(lastEvent === key) return;
+					lastEvent = key;
+					$win.trigger('glass.' + key, [windowWidth]);
+				}
+				
+			}
+		}, debounceTime));
+		
+	};
+	
+	window.glass = Glass;
+	
+})(jQuery, window);
